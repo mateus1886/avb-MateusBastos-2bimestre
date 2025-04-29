3d80406d224744d7be0db38251d44616c1879622
Stefany2510 <stefany.silva2510@eaportal.org>
2025-04-29 12:03:56 -0300

    roteamento de páginas

--- Produtividade/src/App.jsx
+++ Produtividade/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Main from './components/Main'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import AppRoutes from './Routs/AppRoutes'
 
 
 function App() {
@@ -12,19 +13,8 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen">
-      {/* Cabeçalho */}
-      <Header className="bg-blue-500 text-white p-4">
-        <h1 className="text-xl font-bold">Meu Site</h1>
-      </Header>
-
-      {/* Conteúdo principal */}
-      <main className="flex-1 p-6 bg-gray-100">
-        <Main />      
-        </main>
-
-      {/* Rodapé */}
-      <Footer />
+      <div> 
+        <AppRoutes/>
     </div>
     </>
   ) 
---
+++ Produtividade/src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+export default function () {
+    return (
+    <>
+    <h1 className="text-2x1 font-bold">HOME</h1>
+    </>
+    );
+    }
